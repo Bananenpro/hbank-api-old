797306632df911e2e66dcaf62a2e3ffd06188c5e
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-15 15:48:24 +0100

    Add sign depending on who is the sender

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def get_payment_plans(name):
             response.append({
                 "id": p.id,
                 "schedule": p.schedule,
-                "amount": str(p.amount),
+                "amount": str(p.amount) if p.sender_name == name else str(-p.amount),
                 "description": p.desc
             })
         return jsonify(response)
