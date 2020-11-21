5a3ee6d5939053556067b484a9611dd26a922887
Bananenpro05 <julian.hofmann2805@gmail.com>
2020-11-21 11:06:27 +0000

    Add onedrive mount

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cd ~
 sudo apt update && sudo apt upgrade
-sudo apt install python3-pip
+sudo apt install python3-pip rclone
 git clone https://gitlab.com/Bananenpro05/h-bank.git
 cd h-bank
 pip3 install Flask pony Pillow
@@ -10,4 +10,8 @@ sudo systemctl enable hbank.service
 sudo cp system/hbank-payment-plans.service /etc/systemd/system/hbank-payment-plans.service
 sudo cp system/hbank-payment-plans.timer /etc/systemd/system/hbank-payment-plans.timer
 sudo systemctl enable hbank-payment-plans.timer
+mkdir ~/OneDrive
+rclone config
+sudo cp system/onedrive.service /etc/systemd/system/onedrive.service
+sudo systemctl enable onedrive.service
 sudo reboot
