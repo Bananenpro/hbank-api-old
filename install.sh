672d6ec179c745a2e84642be3cb42776a4f4902b
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-21 15:03:48 +0100

    Add dependencies

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ echo "static routers=192.168.0.1" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 echo "static domain_name_servers=192.168.0.1" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 sudo raspi-config
 sudo apt update && sudo apt upgrade
-sudo apt install python3-pip rclone git vim
+sudo apt install python3-pip rclone git vim libopenjp2-7 libtiff5
 sudo reboot
 
