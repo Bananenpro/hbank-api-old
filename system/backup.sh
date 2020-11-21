f6b36a61f2a89fd38a1986733951800b8a4b6301
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-21 13:39:50 +0100

    Update backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -4,5 +4,5 @@ mkdir backup
 cp /home/pi/h-bank/database.sqlite backup/
 cp -r /home/pi/h-bank/uploads/* backup/uploads/
 cd backup
-tar -czpf "/home/pi/OneDrive/H-Bank/Backups/$current_date.tar.gz" .
+tar -czpf "/home/pi/OneDrive/H-Bank/Backups/$current_date.tar.gz" ./
 rm -rf backup
