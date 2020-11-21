b7f4d0a16277e1f5bf80f0892abcaa86acfa403e
Bananenpro05 <julian.hofmann2805@gmail.com>
2020-11-21 09:46:08 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cd ~
+sudo apt update && sudo apt upgrade
+sudo apt install python3-pip
+git clone https://gitlab.com/Bananenpro05/h-bank.git
+cd h-bank
+pip3 install Flask pony Pillow
+sudo cp system/hbank.service /etc/systemd/system/hbank.service
+sudo systemctl enable hbank.service
+sudo cp system/hbank-payment-plans.service /etc/systemd/system/hbank-payment-plans.service
+sudo cp system/hbank-payment-plans.timer /etc/systemd/system/hbank-payment-plans.timer
+sudo systemctl enable hbank-payment-plans.timer
+sudo reboot
