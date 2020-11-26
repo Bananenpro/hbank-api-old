a1f9bbb703a1ad8b4ceb9b684f5ea8f4222fa2eb
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-26 11:42:40 +0100

    Add missing echo

--- system/update.sh
+++ system/update.sh
@@ -2,6 +2,7 @@
 cd ~/h-bank
 echo Updating...
 git pull
+echo Restarting...
 sudo systemctl restart hbank.service
 sudo systemctl restart hbank-backup.timer
 sudo systemctl restart hbank-payment-plans.timer
