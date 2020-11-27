d9643d2d1e4fc163951cfdc3c05f0d81cb4a33e2
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-27 15:04:35 +0100

    Fix missing "/"

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def transfer_money():
         return "", 403
 
 
-@app.route("/payment_plans")
+@app.route("/payment_plans/")
 @app.route("/payment_plans/<string:name>")
 def get_payment_plans(name=""):
     response = []
