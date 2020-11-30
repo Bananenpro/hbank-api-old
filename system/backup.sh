b0bde5d0a04ef2d703a7bb4e5906e4486f625dc0
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-11-30 19:03:08 +0100

    Fix backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 current_date=$(date +"%Y-%m-%d_%H-%M")
 mkdir backup
+mkdir backup/uploads
+mkdir backup/uploads/profile_pictures
 cp /home/pi/h-bank/database.sqlite backup/
 cp -r /home/pi/h-bank/uploads/profile_pictures backup/uploads/profile_pictures
 tar -czpf "/home/pi/OneDrive/H-Bank/Backups/$current_date.tar.gz" backup
