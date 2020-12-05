c3de42a6511d8bb4c123ae537096a1e72fa3d8b4
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-12-05 18:26:48 +0100

    Fix things

--- payment_plans.py
+++ payment_plans.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     payments = database.get_all_payment_plans()
 
     for payment in payments:
-        database.execute_payment_plan(payment.id, True)
+        database.execute_payment_plan(payment.id)
