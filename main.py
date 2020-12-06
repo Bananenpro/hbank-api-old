2113c1b5ae400c9d9fdda4cb414d42b9aa35e0c4
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-12-06 11:43:39 +0100

    Fix load average

--- main.py
+++ main.py
@@ -405,10 +405,7 @@ def info():
     payment_plans = subprocess.run(["systemctl", "status", "hbank-payment-plans.timer"]).returncode == 0
     backups = subprocess.run(["systemctl", "status", "hbank-backup.timer"]).returncode == 0
     temperature = str(round(CPUTemperature().temperature)) + "°C"
-    load_average = round(LoadAverage(minutes=1).value/4*100)
-    if load_average > 100:
-        load_average = 100
-    cpu = str(load_average) + "%"
+    cpu = str(round(LoadAverage(minutes=1).load_average*100)) + "%"
     ram_info = get_ram_info()
     ram = str(round((float(ram_info[1])/float(ram_info[0]))*100)) + "%"
     disk = str(round(DiskUsage().usage)) + "%"
