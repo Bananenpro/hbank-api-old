0abc2eb6120f827c6d8a71824b94cca99fbc373d
Julian Hofmann <julian.hofmann2805@gmail.com>
2020-12-06 11:13:14 +0100

    Restart dhcpcd

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd ~
 echo Setting up the network...
-sudo systemctl enable dhcpcd
-sudo systemctl start dhcpcd
+sudo systemctl enable dhcpcd.service
+sudo systemctl start dhcpcd.service
 echo "interface wlan0" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 echo "static ip_address=192.168.0.200/24" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 echo "static routers=192.168.0.1" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 echo "static domain_name_servers=192.168.0.1" | sudo tee -a /etc/dhcpcd.conf > /dev/null
+sudo systemctl restart dhcpcd.service
 echo Starting raspi-config...
 sudo raspi-config
 echo Installing updates...
